984e4b256c53a2c2fb612f8c48fffea49d2606d2
StevenUster <73930442+StevenUster@users.noreply.github.com>
2021-05-15 14:07:25 +0200

    Update flag.js

--- kussin_custom_flags/js/flag.js
+++ kussin_custom_flags/js/flag.js
@@ -1,3 +1,8 @@
 (function(){
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+  
     $(".grid__item small--one-half medium-up--one-fifth").append("Some appended text.");
 })()
